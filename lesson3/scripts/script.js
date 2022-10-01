2fea14cad47785ec9f3f5b5240541639ba322b1d
dinismubai <mubydinis@gmail.com>
2022-10-01 15:50:17 +0200

    last changes

---
+++ lesson3/scripts/script.js
@@ -0,0 +1,9 @@
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("year").textContent = year;
+
+let lastmodified = document.lastModified;
+document.getElementById("lastmodified").innerHTML = lastmodified
+
+
+
